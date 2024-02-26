18a76427ceccea56f4a32f98e817d6f0dcf4b788
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-26 09:57:25 +0100

    equals

--- src/main/java/com/nttdata/microservicios/app/cursos/models/entity/Curso.java
+++ src/main/java/com/nttdata/microservicios/app/cursos/models/entity/Curso.java
@@ -1,12 +1,18 @@
 package com.nttdata.microservicios.app.cursos.models.entity;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+
+import com.nttdata.microservicios.commons.alumnos.models.entity.Alumno;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.Table;
 import jakarta.persistence.Temporal;
@@ -23,13 +29,35 @@ public class Curso {
 	@Column(name="create_at")
 	@Temporal(TemporalType.TIMESTAMP)
 	private Date createAt;
+	@OneToMany(fetch = FetchType.LAZY)
+	private List<Alumno> alumnos;
 	
+	public List<Alumno> getAlumnos() {
+		return alumnos;
+	}
+
+
+	public void setAlumnos(List<Alumno> alumnos) {
+		this.alumnos = alumnos;
+	}
+	public void addAlumno(Alumno alumno) {
+		this.alumnos.add(alumno);
+	}
+	public void removeAlumno(Alumno alumno) {
+		this.alumnos.remove(alumno);
+	}
+
 	@PrePersist
 	public void prePersist() {
 		this.createAt = new Date();
 	}
 	
 	
+	public Curso() {
+		this.alumnos = new ArrayList<>();
+	}
+
+
 	public Long getId() {
 		return id;
 	}
