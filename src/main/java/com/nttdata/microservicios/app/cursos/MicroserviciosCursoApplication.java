1a35561eb854f5d00127f0ed3fcc8f25b4693333
ipastorl <isabel.pastorlopez@emeal.nttdata.com>
2024-02-26 09:36:22 +0100

    dependencia alumno entity

--- src/main/java/com/nttdata/microservicios/app/cursos/MicroserviciosCursoApplication.java
+++ src/main/java/com/nttdata/microservicios/app/cursos/MicroserviciosCursoApplication.java
@@ -3,9 +3,12 @@ package com.nttdata.microservicios.app.cursos;
 import org.springframework.boot.SpringApplication;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
 
 @SpringBootApplication
+@EntityScan({"com.nttdata.microservicios.commons.alumnos.models.entity",
+			 "com.nttdata.microservicios.app.cursos.models.entity"})
 public class MicroserviciosCursoApplication {
 
 	public static void main(String[] args) {
